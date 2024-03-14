b5685fef2e458c286659d46a36eb090a889c4ce0
wodn1478 <wodn14789@naver.com>
2024-03-14 23:37:10 +0900

    refactor : 닉네임 가져오기 엔드포인트 수정 #4
    Github issue #4

--- src/main/java/com/pyre/feed/clients/UserClient.java
+++ src/main/java/com/pyre/feed/clients/UserClient.java
@@ -3,10 +3,11 @@ package com.pyre.feed.clients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(value = "auth-service", path = "/auth-service/user")
 public interface UserClient {
-    @GetMapping("/get/nickname")
-    ResponseEntity<String> getNickname(@RequestHeader("id") String userId);
+    @GetMapping("/get/nickname/{userId}")
+    ResponseEntity<String> getNickname(@PathVariable String userId);
 }
